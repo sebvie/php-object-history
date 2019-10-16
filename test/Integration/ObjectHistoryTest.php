7931a2b8c87037ae4a1679dd7df278f4ebaae2e1
sebastianviereck <info@sebastianviereck.de>
2019-10-16 10:53:20 +0200

    test running

--- test/Integration/ObjectHistoryTest.php
+++ test/Integration/ObjectHistoryTest.php
@@ -1,11 +1,27 @@
 <?php
 
-namespace SebVie\PhpObjectHistory\Tests;
+namespace SebVie\PhpObjectHistory\Tests\Integration;
 
 use PHPUnit\Framework\TestCase;
+use SebVie\PhpObjectHistory\ObjectHistory;
 
 class ObjectHistoryTest extends TestCase
 {
+    /**
+     * @var ObjectHistory
+     */
+    protected $subject;
+
+
+    /**
+     * @return void
+     */
+    public function setup(): void
+    {
+        parent::setUp();
+        $this->subject = new ObjectHistory();
+    }
+
     public function test1()
     {
         $this->assertTrue(true);
