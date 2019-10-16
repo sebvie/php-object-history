8ddd13835a909d094214b98e19ffb197c7d2549c
sebastianviereck <info@sebastianviereck.de>
2019-10-16 10:43:22 +0200

    test running

---
+++ test/Integration/ObjectHistoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SebVie\PhpObjectHistory\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ObjectHistoryTest extends TestCase
+{
+    public function test1()
+    {
+        $this->assertTrue(true);
+    }
+}
