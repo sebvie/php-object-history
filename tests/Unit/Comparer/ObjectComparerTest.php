090ffc779e549e6907ca4bedcb63372414acd352
sebastianviereck <info@sebastianviereck.de>
2019-10-16 16:26:37 +0200

    compare object

--- tests/Unit/Comparer/ObjectComparerTest.php
+++ tests/Unit/Comparer/ObjectComparerTest.php
@@ -2,13 +2,15 @@
 
 namespace PhpObjectComparer\Tests\Unit\Comparer;
 
-use PhpObjectHistory\Entity\ObjectChange;
 use PHPUnit\Framework\TestCase;
 use PhpObjectHistory\Comparer\ObjectComparer;
 use PhpObjectHistory\Tests\Fixture\ObjectClassFixture;
 
 class ObjectComparerTest extends TestCase
 {
+
+    const FIXTURE_PROPERTY_COUNT = 9;
+
     /**
      * @var ObjectComparer
      */
@@ -22,6 +24,38 @@ class ObjectComparerTest extends TestCase
     {
         parent::setUp();
         $this->subject = new ObjectComparer();
+    }
+
+    /**
+     * @return void
+     */
+    public function testCompareEmptyOldValue(): void
+    {
+        $oldValue = new \stdClass();
+        $newValue = new ObjectClassFixture();
+
+        $result = $this->subject->getDiff($oldValue, $newValue);
+
+        $this->assertCount(self::FIXTURE_PROPERTY_COUNT, $result);
+        foreach ($result as $objectChange) {
+            $this->assertNull($objectChange->getOldValue());
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function testCompareEmptyNewValue(): void
+    {
+        $oldValue = new ObjectClassFixture();
+        $newValue = new \stdClass();
+
+        $result = $this->subject->getDiff($oldValue, $newValue);
+
+        $this->assertCount(self::FIXTURE_PROPERTY_COUNT, $result);
+        foreach ($result as $objectChange) {
+            $this->assertNull($objectChange->getNewValue());
+        }
     }
 
     /**
@@ -43,6 +77,52 @@ class ObjectComparerTest extends TestCase
         $objectChange = $result[0];
 
         $this->assertEquals('privateProperty', $objectChange->getAttribute());
+        $this->assertEquals($oldValueChange, $objectChange->getOldValue());
+        $this->assertEquals($newValueChange, $objectChange->getNewValue());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCompareProtectedAttribute(): void
+    {
+        $oldValueChange = 1;
+        $newValueChange = 2;
+        $oldValue = new ObjectClassFixture();
+        $oldValue->setProtectedProperty($oldValueChange);
+        $newValue = new ObjectClassFixture();
+        $newValue->setProtectedProperty($newValueChange);
+
+        $result = $this->subject->getDiff($oldValue, $newValue);
+
+        $this->assertCount(1, $result);
+
+        $objectChange = $result[0];
+
+        $this->assertEquals('protectedProperty', $objectChange->getAttribute());
+        $this->assertEquals($oldValueChange, $objectChange->getOldValue());
+        $this->assertEquals($newValueChange, $objectChange->getNewValue());
+    }
+
+    /**
+     * @return void
+     */
+    public function testComparePublicAttribute(): void
+    {
+        $oldValueChange = 1;
+        $newValueChange = 2;
+        $oldValue = new ObjectClassFixture();
+        $oldValue->setPublicProperty($oldValueChange);
+        $newValue = new ObjectClassFixture();
+        $newValue->setPublicProperty($newValueChange);
+
+        $result = $this->subject->getDiff($oldValue, $newValue);
+
+        $this->assertCount(1, $result);
+
+        $objectChange = $result[0];
+
+        $this->assertEquals('publicProperty', $objectChange->getAttribute());
         $this->assertEquals($oldValueChange, $objectChange->getOldValue());
         $this->assertEquals($newValueChange, $objectChange->getNewValue());
     }
